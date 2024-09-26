c2e8a8d2fde5513a15e23317e9867492569e5d55
Razeenali <razeenali22@gmail.com>
2024-09-26 15:57:15 -0400

    Added sample expressions

--- src/main/java/com/pluralsight/Demo1.java
+++ src/main/java/com/pluralsight/Demo1.java
@@ -4,6 +4,33 @@ public class Demo1 {
 
     public static void main(String [] args){
         System.out.println("Hello World");
+
+       //int identifer = 1;
+        String name = "Razeen";
+
+
+        String greeting = "Hello " + name;
+
+        int number_of_days_per_week_watches_TV = 5;
+        int minsPerDayOnAverage = 70;
+
+        int minsPerWeek = number_of_days_per_week_watches_TV * minsPerDayOnAverage;
+        int minPerYear = minsPerWeek * 52;
+
+        int hoursPerYear = minPerYear / 60;
+
+        int hoursInWorkWeek = 35;
+
+        int workWeeksPerYearWatchingTV = hoursPerYear / hoursInWorkWeek;
+
+        int workWeeksPerYearWatchingTV2 = (5 * 70  *  52 / 60 ) / 35;
+
+        System.out.println("If "+name+ " did not watch TV, it would be like a " + workWeeksPerYearWatchingTV + " Week vacation every year.");
+
+        System.out.println("If "+name+ " did not watch TV, it would be like a " + workWeeksPerYearWatchingTV2 + " Week vacation every year.");
+
+        System.out.println(greeting);
+
     }
 
 }
