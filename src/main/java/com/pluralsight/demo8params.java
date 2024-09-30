167288f354a8f722924139df1acd87fbd07313de
Razeenali <razeenali22@gmail.com>
2024-09-30 13:07:28 -0400

    added methods and params

---
+++ src/main/java/com/pluralsight/demo8params.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class demo8params {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 9;
+        int c = 10;
+        int d = 3;
+
+        addAndDisplay(a, b);
+        addAndDisplay(b, c);
+        addAndDisplay(a, d);
+    }
+    public static void addAndDisplay(int num1, int num2) {
+        int sum = num1 + num2;
+        System.out.printf("%d + %d = %d\n", num1, num2, sum);
+        //System.out.println();
+    }
+}
